a0444353a3ac77f57c954a2ede26246401f3fc34
周德铭 <784106968@qq.com>
2024-02-28 17:06:04 +0800

    添加二叉树层序遍历和层序创建功能，并通过所有功能测试

---
+++ Algorithm/SortAlgorithm/QuickSort.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 78410 on 2024/2/28.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
--- Tree/Binary Tree.cpp
+++ Tree/Binary Tree.cpp
@@ -27,6 +27,49 @@ void creatTree(BiTree& t){
     }
 }
 
+//层序创建二叉树：
+//与遍历类似，由队列控制数据的进出
+void OrderCreatTree(BiTree& t){
+    char ch;
+    cin>>ch;
+    queue<BiTree>q;
+    if(ch!='#'){
+        t=new BiNode;
+        t->data=ch;
+        q.push(t);
+    }
+    while(!q.empty()){
+        //输入字符，判断左子树是否有数据
+        cin>>ch;
+        //存在数据，则开辟新节点，并入队列
+        if(ch!='#'){
+            BiNode *p=q.front();
+            p->lchild=new BiNode;
+            p->lchild->data=ch;
+            q.push(p->lchild);
+        }
+        //不存在数据，直接置空
+        else{
+            q.front()->lchild=NULL;
+        }
+        //输入字符，判断右子树是否有数据
+        cin>>ch;
+        //右子树存在数据，开辟新节点，并入队列
+        if(ch!='#'){
+            BiNode *p=q.front();
+            p->rchild=new BiNode;
+            p->rchild->data=ch;
+            q.push(p->rchild);
+        }
+        //不存在数据，直接置空
+        else{
+            q.front()->rchild=NULL;
+        }
+        //最后不要忘记队头节点出队
+        q.pop();
+    }
+}
+
 //先序递归遍历：
 void PreOrder(BiNode* t){
     if(t==NULL){
@@ -57,7 +100,23 @@ void PostOrder(BiNode* t){
     cout<<t->data;
 }
 
-
+//层序遍历二叉树：
+//思路：建立先进先出队列，首先入队根节点，若队列不为空则执行如下循环：
+//输出队头节点的数据，判断队头节点的左右孩子是否存在，存在则入队，判断结束后，将当前根节点（队头节点）出队
+void Order(BiTree t){
+    queue<BiTree>q;
+    q.push(t);
+    while(!q.empty()){
+        cout<<q.front()->data;
+        if(q.front()->lchild!=NULL){
+            q.push(q.front()->lchild);
+        }
+        if(q.front()->rchild!=NULL){
+            q.push(q.front()->rchild);
+        }
+        q.pop();
+    }
+}
 
 //统计二叉树中节点个数：
 //自底向上，首先递归到最底层，以叶子节点为例，叶子节点左右子树都递归为0，此时可计数叶子节点为1
@@ -118,8 +177,36 @@ bool isEmpty(BiTree t){
 
 int main(){
     BiTree t;
-    creatTree(t);
-
-
+    //先序创建二叉树
+    //creatTree(t);
+    //层序创建二叉树：
+    OrderCreatTree(t);
+    //先序遍历结果：
+    cout<<"先序遍历：";
+    PreOrder(t);
+    cout<<endl;
+    //中序遍历结果：
+    cout<<"中序遍历：";
+    InOrder(t);
+    cout<<endl;
+    //后序遍历结果：
+    cout<<"后序遍历：";
+    PostOrder(t);
+    cout<<endl;
+    //层序遍历结果：
+    cout<<"层序遍历：";
+    Order(t);
+    cout<<endl;
+    //统计二叉树中的节点个数：
+    cout<<"二叉树中的节点个数为："<<NodeCount(t)<<endl;
+    //统计二叉树的深度：
+    cout<<"二叉树的深度为："<<Depth(t)<<endl;
+    //统计二叉树的叶子节点个数：
+    cout<<"二叉树的叶子节点个数为："<<LeafCount(t)<<endl;
+    //复制二叉树：
+    BiTree Newt;
+    Copy(t,Newt);
+    PreOrder(Newt);
+    cout<<endl;
     return 0;
 }
